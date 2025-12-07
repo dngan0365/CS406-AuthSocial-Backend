4fa7ae71d177489d8a6c532f068c3d391d6dddaf
dngan0365 <dngan0365@gmail.com>
2025-12-07 23:44:43 +0700

    fix: path file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from backend.config import get_settings
+from config import get_settings
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
